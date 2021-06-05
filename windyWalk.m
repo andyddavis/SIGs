ebdff2bdd89840efd13013fb38036f7066ce8ecc
Sean Mackin <seanamackin15@gmail.com>
2021-06-05 14:33:22 -0500

    Update windyWalk.m

--- windyWalk.m
+++ windyWalk.m
@@ -1,5 +1,8 @@
+clear
+% number of nodes in the line
 n = 9;
 
+% initialise the transition matrix
 tMatrix = zeros(n,n);
 for i = 1 : n
     tMatrix(i,i) = 1/3;
@@ -12,13 +15,16 @@ end
 tMatrix(n,n-1)=1/3;
 tMatrix(n,n) = 2/3;
 
-A = 9;
+% initialise the agents on the far left
+A = 20;
 agents = ones(1,A);
 
-time = 20;
+% simulation data
+time = 50;
 dt = 1;
 timesteps = time/dt;
 
+% live plotting
 for timestep = 1 : timesteps
     
     for agent = 1 : A
@@ -33,14 +39,8 @@ for timestep = 1 : timesteps
     end
     
     histogram(agents,n,'BinWidth', 1);
-    %plot(agents);
     xlim([1,n+1]);
-    ylim([1,A+1]);
-    pause(1);
+    ylim([0,A+1]);
+    pause(0.5);
     
 end
-
-histogram(agents,n,'BinWidth', 1);
-  xlim([1,n+1]);
-  ylim([1,A+1]);
-  
