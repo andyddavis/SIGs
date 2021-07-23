506ad6588fff196f2c617457fb59f4cce4df5d7e
Andy Davis <davisad@alum.mit.edu>
2021-07-23 09:56:02 -0400

    Added documentation to Domain.py

--- sig/Domain.py
+++ sig/Domain.py
@@ -18,11 +18,35 @@ plt.rcParams['legend.fontsize'] = 14
 
 
 class Domain:
+    """A class that defines all of the functions required to define our sea ice model"""
+
     def __init__(self, x_lim=(0.0, 1.0), y_lim=(0.0, 1.0)):
+        """
+        Assumes a square domain with potentially different x and y limits.
+        Parameters
+        ----------
+        x_lim : tuple
+            The x limits
+        y_lim : tuple
+            The y limits
+        """
         self.x_lim = x_lim
         self.y_lim = y_lim
 
     def directional_distance(self, point1, point2):
+        """
+        Returns the distance between two points and the vector that points between them assuming a doubly-periodic domain.
+        Parameters
+        ----------
+        point1 :
+            The first point.
+        point2 :
+            The second point.
+
+        Returns
+        ----------
+        The distance between the two points and a unit vector that points from point1 to point2. Moving along that vector is the shortest path from point1 to point2
+        """
         xlength = self.x_lim[1]-self.x_lim[0]
         ylength = self.y_lim[1]-self.y_lim[0]
 
@@ -37,30 +61,32 @@ class Domain:
 
         return vals[ind], vecs[ind]/vals[ind] if vals[ind]>1.0e-12 else [0.0, 0.0]
 
-    # define external velocity (vector field) function
     def external_velocity(self, x, y):
-        return (1.0, 0.0)
+        """The external velocity that the ice feels from the ocean and atmosphere. The default is to return (0,0)."""
+        return (0.0, 0.0)
 
-    # collision rate function
     def collision_rate(self, x, y):
-        return (0.5 if x<0.5 else 1.5)
+        """The collision rate function that defines the frequently particles collide in the collision operator. Defaults to 1."""
+        return 1.0
 
     def mean_free_path(self, x, y):
+        """The nondimensional operator that multiplies the collision operator. Small mean free path means more frequent collisions and larger mean free path means less frequent collisions. Must be between 0 and 1. Defaults to 1e-2."""
         return 1.0e-2
 
     def energy_loss_ratio(self, x, y):
-        return 0.8
+        """The amount of energy remaining after a two-particle collision. If it is less than one, energy is lost. If it is greater than one, energy is gained. If it is equal to 1, energy is conserved. Defaults to 1."""
+        return 1
 
-    # domain area
     def area(self):
+        """The area of the entire domain"""
         return (self.x_lim[1]-self.x_lim[0])*(self.y_lim[1]-self.y_lim[0])
 
-    # initial mass density---defaults to constant
     def initial_mass_density(self, x, y):
+        """The initial mass density---used to initially distribute the particles. Must integrate to 1"""
         return 1.0/self.area()
 
-    # external velocity plot
     def plot_external_velocity(self, filename='external_velocity', ext='pdf', narrows=15):
+        """Plot the external velocity field."""
         x, y = np.meshgrid(np.linspace(self.x_lim[0], self.x_lim[1], narrows), np.linspace(self.y_lim[0], self.y_lim[1], narrows))
         (u, v) = self.external_velocity(x, y)
 
