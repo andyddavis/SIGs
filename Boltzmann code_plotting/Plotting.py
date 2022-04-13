cf899b6709573f15c35c9b97b0e1a08493eaa8a2
Andy Davis <davisad@alum.mit.edu>
2022-04-13 13:34:35 -0400

    added plotting functions

---
+++ Boltzmann code_plotting/Plotting.py
@@ -0,0 +1,73 @@
+from Graph import *
+
+# import plotting packages and set default figure options
+useserif = True # use a serif font with figures?
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, BoundaryNorm
+if useserif:
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams['text.usetex'] = False
+else:
+    plt.rcParams["font.family"] = "sans-serif"
+    plt.rcParams['text.usetex'] = False
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['xtick.labelsize'] = 18
+plt.rcParams['ytick.labelsize'] = 18
+plt.rcParams['legend.fontsize'] = 18
+
+
+def PlotGamma(t, graph, direc):
+    fig = plt.figure(figsize = (40,30), clear = True)
+    Gamma = graph.Gamma()
+    ax = plt.gca()
+    pc = ax.pcolormesh(graph.x, graph.y, Gamma.T, shading='auto', cmap='jet')
+    fig.colorbar(pc)
+    plt.savefig(direc+'gamma/'+'fig_gamma-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
+    fig.clear()
+    plt.close(fig)
+
+def PlotPETE(t, graph, direc):
+    # plot PETE
+    fig = plt.figure(figsize = (40,30), clear = True)
+    PETE = graph.PE_TE()
+    ax = plt.gca()
+    pc = ax.pcolormesh(graph.x, graph.y, PETE.T, shading='auto', cmap='jet')
+    fig.colorbar(pc)
+    plt.savefig(direc+'PETE/'+'fig_PETE-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
+    fig.clear()
+    plt.close(fig)
+
+def PlotKETE(t, graph, direc):
+    # plot KETE 
+    fig = plt.figure(figsize = (40,30), clear = True)
+    KETE = graph.KE_TE()
+    ax = plt.gca()
+    pc = ax.pcolormesh(graph.x, graph.y, KETE.T, shading='auto', cmap='jet')
+    fig.colorbar(pc)
+    plt.savefig(direc+'KETE/'+'fig_KETE-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
+    fig.clear()
+    plt.close(fig)
+
+def PlotMass(t, X, Y, massDensity, graph, direc):
+    fig = plt.figure(figsize = (40,30), clear = True)
+    ax = plt.gca()
+    pc = ax.pcolormesh(graph.x, graph.y, massDensity.T, shading='auto', cmap='jet')
+    # print expected velocities
+    U=[];V=[]
+    for node in graph.nodes:
+        U.append(node.ev[0]); V.append(node.ev[1])
+    plt.quiver(X,Y,U,V)
+    #pc = ax.pcolormesh(graph.x, graph.y, massDensity.T, shading='gouraud', cmap='jet')
+    fig.colorbar(pc)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.set_ylabel(r'$y$')
+    ax.set_xlabel(r'$x$')
+    #ax.set_xlim([x[0], x[-1]])
+    #ax.set_ylim([0, 1.1*max(xmarginal)])
+    plt.savefig(direc+'mass/'+'fig_mass-density-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
+    fig.clear()
+    plt.close(fig)
--- Boltzmann code_plotting/_run.py
+++ Boltzmann code_plotting/_run.py
@@ -3,16 +3,15 @@ import scipy.sparse as sparse
 
 import h5py as h5
 
-import os
+import shutil, os
 
 from Graph import *
-
+from Plotting import *
 
 from ttictoc import tic,toc
 
 import time
 
-tic()
 # import plotting packages and set default figure options
 useserif = True # use a serif font with figures?
 import matplotlib as mpl
@@ -29,6 +28,9 @@ plt.rcParams['xtick.labelsize'] = 14
 plt.rcParams['ytick.labelsize'] = 14
 plt.rcParams['legend.fontsize'] = 14
 
+
+tic()
+
 nx = 50 # number of nodes in the x direction
 ny = 50 # number of nodes in the y direction
 
@@ -37,8 +39,15 @@ Ly = 1.0 # length of the domain in the y direction
 
 m = int(1e5) # the number of particles
 
-direc = '/Users/25016/OneDrive/桌面/simu0311/copy/'  #accelaration_0.3_collision_rate_KETE
-# direc = '/Users/lm4307/Desktop/simu0311/p_1.5/' 
+#direc = '/Users/25016/OneDrive/桌面/simu0311/copy/'  #accelaration_0.3_collision_rate_KETE
+#direc = '/Users/lm4307/Desktop/simu0311/p_1.5/' 
+direc = 'figures/'
+
+qois = ['gamma', 'PETE', 'KETE', 'mass']
+for qoi in qois:
+    if os.path.isdir('figures/'+qoi):
+        shutil.rmtree('figures/'+qoi)
+    os.mkdir('figures/'+qoi)
 
 # create a regular rectangular graph with m particles
 graph = Graph(nx, Lx, ny, Ly, m)
@@ -48,11 +57,15 @@ nsteps = 1500
 X =[];Y=[]
 plot_or_not = 0
 
-skips = 5 # plot every "skips" time steps
+skips = 1 # plot every "skips" time steps
 
 # get the location of the nodes
 for node in graph.nodes:
         X.append(node.x); Y.append(node.y)
+
+# the initial mass density
+massDensity = graph.MassDensity()
+
 for t in range(nsteps):
     if t%skips ==0:
         plot_or_not =1
@@ -63,69 +76,21 @@ for t in range(nsteps):
     for node in graph.nodes:
         node.update_energy()
         
-    if plot_or_not:
-        
-        # plot gamma 
-        fig1 = plt.figure(figsize = (40,30), clear = True)
-        Gamma = graph.Gamma()
-        ax = plt.gca()
-        pc = ax.pcolormesh(graph.x, graph.y, Gamma.T, shading='auto', cmap='jet')
-        fig1.colorbar(pc)
-        plt.savefig(direc+'gamma/'+'fig_gamma-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
-        fig1.clear()
-        plt.close(fig1)
-        # plot PETE
-        fig2 = plt.figure(figsize = (40,30), clear = True)
-        PETE = graph.PE_TE()
-        ax = plt.gca()
-        pc = ax.pcolormesh(graph.x, graph.y, PETE.T, shading='auto', cmap='jet')
-        fig2.colorbar(pc)
-        plt.savefig(direc+'PETE/'+'fig_PETE-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
-        fig2.clear()
-        plt.close(fig2)
-        # plot KETE 
-        fig3 = plt.figure(figsize = (40,30), clear = True)
-        KETE = graph.KE_TE()
-        ax = plt.gca()
-        pc = ax.pcolormesh(graph.x, graph.y, KETE.T, shading='auto', cmap='jet')
-        fig3.colorbar(pc)
-        plt.savefig(direc+'KETE/'+'fig_KETE-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
-        fig3.clear()
-        plt.close(fig3)
+    if plot_or_not:        
+        PlotGamma(t, graph, direc)
+        PlotPETE(t, graph, direc)
+        PlotKETE(t, graph, direc)
+        PlotMass(t, X, Y, massDensity, graph, direc)
+        plt.close('all')
+
     print('\tstarting convection step.')
     graph.ConvectionStep(dt)
     print('\tfinished convection step.')
     print('\tstarting collision step.')
+    # update the mass density
     massDensity = graph.MassDensity()
     graph.CollisionStep(massDensity, dt, idx)
     print('\tfinished collision step.')
-    if plot_or_not:
-        # plot mass
-        
-        fig = plt.figure(figsize = (40,30), clear = True)
-        ax = plt.gca()
-        pc = ax.pcolormesh(graph.x, graph.y, massDensity.T, shading='auto', cmap='jet')
-        # print expected velocities
-        U=[];V=[]
-        for node in graph.nodes:
-            U.append(node.ev[0]); V.append(node.ev[1])
-        plt.quiver(X,Y,U,V)
-        #pc = ax.pcolormesh(graph.x, graph.y, massDensity.T, shading='gouraud', cmap='jet')
-        fig.colorbar(pc)
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.yaxis.set_ticks_position('left')
-        ax.xaxis.set_ticks_position('bottom')
-        ax.set_ylabel(r'$y$')
-        ax.set_xlabel(r'$x$')
-        #ax.set_xlim([x[0], x[-1]])
-        #ax.set_ylim([0, 1.1*max(xmarginal)])
-        
-        plt.savefig(direc+'mass/'+'fig_mass-density-'+str(t).zfill(6)+'.png', format='png', bbox_inches='tight')
-        fig.clear()
-        plt.close(fig)
-        
-    plt.close('all')
 
 total_time = toc()
 print('Total time for the simulation: ', total_time)
